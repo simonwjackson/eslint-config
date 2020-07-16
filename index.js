4d5d32dc8ce4794189648dde44e7cc23d96a274f
Simon W. Jackson <simon@simonwjackson.com>
2020-07-16 17:15:39 -0500

    feat: add unicorn plugin

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ module.exports = {
     "node": true
   },
   "plugins": [
+    "unicorn", 
     "unused-imports",
     "simple-import-sort",
     "fp",
@@ -54,8 +55,11 @@ module.exports = {
     }
   },
   "rules": {
+    // React
     "react/jsx-uses-react": "error",
     "react/jsx-uses-vars": "error",
+
+    // FP
     "fp/no-arguments": "error",
     "fp/no-class": "error",
     "fp/no-delete": "error",
@@ -70,7 +74,7 @@ module.exports = {
       {
         "commonjs": true
       }
-    ],
+    ], 
     "fp/no-nil": "error",
     "fp/no-proxy": "error",
     "fp/no-rest-parameters": "error",
@@ -79,15 +83,79 @@ module.exports = {
     "fp/no-unused-expression": "error",
     "fp/no-valueof-field": "error",
     "no-var": "error",
+     
+    // Unicorn
+    "unicorn/better-regex": "error",
+    "unicorn/catch-error-name": "error",
+    "unicorn/consistent-function-scoping": "error",
+    "unicorn/custom-error-definition": "off",
+    "unicorn/error-message": "error",
+    "unicorn/escape-case": "error",
+    "unicorn/expiring-todo-comments": "error",
+    "unicorn/explicit-length-check": "error",
+    "unicorn/filename-case": "error",
+    "unicorn/import-index": "error",
+    "unicorn/new-for-builtins": "error",
+    "unicorn/no-abusive-eslint-disable": "error",
+    "unicorn/no-array-instanceof": "error",
+    "unicorn/no-console-spaces": "error",
+    "unicorn/no-fn-reference-in-iterator": "error",
+    "unicorn/no-for-loop": "error",
+    "unicorn/no-hex-escape": "error",
+    "unicorn/no-keyword-prefix": "off",
+    "no-nested-ternary": "off",
+    "unicorn/no-nested-ternary": "error",
+    "unicorn/no-new-buffer": "error",
+    "unicorn/no-null": "error",
+    "unicorn/no-object-as-default-parameter": "error",
+    "unicorn/no-process-exit": "error",
+    "unicorn/no-reduce": "error",
+    "unicorn/no-unreadable-array-destructuring": "error",
+    "unicorn/no-unsafe-regex": "off",
+    "unicorn/no-unused-properties": "off",
+    "unicorn/no-useless-undefined": "error",
+    "unicorn/no-zero-fractions": "error",
+    "unicorn/number-literal-case": "error",
+    "unicorn/prefer-add-event-listener": "error",
+    "unicorn/prefer-array-find": "error",
+    "unicorn/prefer-dataset": "error",
+    "unicorn/prefer-event-key": "error",
+    "unicorn/prefer-flat-map": "error",
+    "unicorn/prefer-includes": "error",
+    "unicorn/prefer-modern-dom-apis": "error",
+    "unicorn/prefer-negative-index": "error",
+    "unicorn/prefer-node-append": "error",
+    "unicorn/prefer-node-remove": "error",
+    "unicorn/prefer-number-properties": "error",
+    "unicorn/prefer-optional-catch-binding": "error",
+    "unicorn/prefer-query-selector": "error",
+    "unicorn/prefer-reflect-apply": "error",
+    "unicorn/prefer-replace-all": "off",
+    "unicorn/prefer-set-has": "error",
+    "unicorn/prefer-spread": "error",
+    "unicorn/prefer-starts-ends-with": "error",
+    "unicorn/prefer-string-slice": "error",
+    "unicorn/prefer-text-content": "error",
+    "unicorn/prefer-trim-start-end": "error",
+    "unicorn/prefer-type-error": "error",
+    "unicorn/prevent-abbreviations": "error",
+    "unicorn/string-content": "off",
+    "unicorn/throw-new-error": "error",
+
+    // Jest
     "jest/no-disabled-tests": "warn",
     "jest/no-focused-tests": "error",
     "jest/no-identical-title": "error",
     "jest/prefer-to-have-length": "warn",
     "jest/valid-expect": "error",
+
     "modules-newline/import-declaration-newline": "error",
     "modules-newline/export-declaration-newline": "error",
+
     "simple-import-sort/sort": "error",
+
     "unused-imports/no-unused-imports": "error",
+
     "object-curly-newline": [
       "error",
       {
