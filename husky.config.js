b2893fdde34ecee1c381f1e57128b25568507c95
Simon W. Jackson <simon@simonwjackson.com>
2020-07-16 17:05:10 -0500

    build: remove cz dependency

--- husky.config.js
+++ husky.config.js
@@ -1,5 +1,1 @@
-module.exports = {
-  "hooks": {
-    "prepare-commit-msg": "exec < /dev/tty && git cz --hook || true" 
-  }
-}
+module.exports = { hooks: { 'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS' } }
