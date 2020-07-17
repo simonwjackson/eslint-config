b119c8f1d4a4fa8a5928f785e8700f21fa86b092
Simon W. Jackson <simon@simonwjackson.com>
2020-07-17 13:29:17 -0500

    ci: switch to sematic release

---
+++ .releaserc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  "branches": ["master"],
+  "plugins": [
+    ["@semantic-release/commit-analyzer", {
+      "preset": "angular",
+      "releaseRules": [
+        {"type": "docs", "scope":"README", "release": "patch"},
+        {"type": "refactor", "release": "patch"},
+        {"type": "style", "release": "patch"}
+      ],
+      "parserOpts": {
+        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
+      }
+    }],
+    ["@semantic-release/release-notes-generator", {
+      "preset": "angular",
+      "parserOpts": {
+        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+      },
+      "writerOpts": {
+        "commitsSort": ["subject", "scope"]
+      }
+    }],
+    ["@semantic-release/changelog", {
+      "changelogFile": "docs/CHANGELOG.md",
+    }],
+    ["@semantic-release/npm", {
+      "npmPublish": true,
+      // "tarballDir": "dist",
+      // "pkgRoot": "out",
+    }],
+    ["@semantic-release/github", {
+      "assets": "*.tgz"
+    }],
+    // "@qiwi/semantic-release-gh-pages-plugin"
+  ]
+}
